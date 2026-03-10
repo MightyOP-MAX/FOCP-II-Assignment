491f73c38cd0e76cdbe3c067fa6f5e4f76f82a48
MightyOP-MAX <aman.raghav7862@gmail.com>
2026-03-10 13:39:17 +0530

    Q6

---
+++ Q7.cpp
@@ -0,0 +1,14 @@
+/*Q7. A game compares three players' scores to find who is ahead. Implement a solution to accept three scores 
+and identify the winner.*/
+#include<iostream>
+using namespace std;
+int main() {
+
+
+
+
+
+
+
+
+}
