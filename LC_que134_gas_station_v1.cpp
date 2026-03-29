6408ae2494c838adeaae0be242d952bbdea79172
MightyOP-MAX <aman.raghav7862@gmail.com>
2026-03-29 13:34:48 +0530

    saving

--- LC_que134_gas_station_v1.cpp
+++ LC_que134_gas_station_v1.cpp
@@ -2,29 +2,30 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int canCompleteCircuit(int gas[],int cost[] , int n) 
+    int canCompleteCircuit(int gas[], int cost[], int n)
     {
         int totalGas = 0;
         int totalCost = 0;
         int tank = 0;
         int start = 0;
 
-        for (int i = 0; i < n; i++) 
+        for (int i = 0; i < n; i++)
         {
             totalGas = totalGas + gas[i];
             totalCost = totalCost + cost[i];
             tank = tank + gas[i] - cost[i];
 
-            if (tank < 0) 
+            if (tank < 0)
             {
-                start = i +1;
+                start = i + 1;
                 tank = 0;
             }
         }
 
-        if (totalGas < totalCost) 
+        if (totalGas < totalCost)
         {
             return -1;
         }
@@ -33,7 +34,8 @@ public:
     }
 };
 
-int main() {
+int main()
+{
     int gas[] = {1, 2, 3, 4, 5};
     int cost[] = {3, 4, 5, 1, 2};
     int n = 5;
