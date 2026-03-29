69d610879fde3ddb3b323352bb91a58781b74e3b
MightyOP-MAX <aman.raghav7862@gmail.com>
2026-03-29 13:09:42 +0530

    leet code question done

---
+++ Q3.cpp
@@ -0,0 +1,38 @@
+/* Q3. Write a C++ program to create a class Rectangle with length and breadth
+ as data members.
+Use a constructor to initialize the dimensions and calculate the area of the rectangle.*/
+
+#include<iostream>
+#include<string>
+using namespace std;
+class Rectangle{
+private:
+float length;
+float breadth;
+public:
+Rectangle();
+Rectangle(float ,float );
+float calculatearea(){
+    return (length*breadth);
+}
+void displaydetails(){
+    cout<<"\narea is: "<<calculatearea();
+}
+};
+Rectangle::Rectangle(){
+    cout<<"\ndefault parameter was called";
+    length=0;
+    breadth=0;
+}
+Rectangle::Rectangle(float length,float breadth){
+    this->length=length;
+    this->breadth=breadth;
+}
+int main(){  
+Rectangle r1,r2(2.2f,2.2f);
+
+//r1.calculatearea();
+r1.displaydetails();
+r2.displaydetails();
+return 0;
+}
