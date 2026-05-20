bd9172d528a7e462a2b61406de511d03a28510a4
MightyOP-MAX <aman.raghav7862@gmail.com>
2026-05-20 17:47:30 +0530

    done

--- debugging_challenge_31_march.cpp
+++ debugging_challenge_31_march.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     ios::sync_with_stdio(false);
@@ -8,20 +7,19 @@ int main()
 
     int T, N, x;
     cin >> T;
-
     while (T > 0)
     {
         cin >> N;
-
         int alive[500000], size = 0;
-
         for (int i = 0; i < N; i++)
         {
             cin >> x;
 
             // kill all monsters with strength <= x
             while (size > 0 && alive[size - 1] <= x)
+            {
                 size--;
+            }
 
             alive[size++] = x; // new monster joins
 
